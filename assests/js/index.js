6fc3dd821461ee0ea7a67d2021b967d751946120
SumaiaSorna <sorna.sumaia@gmail.com>
2021-10-13 16:34:58 +0100

    used moment js to get today's date

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,1 +1,6 @@
+$(document).ready(function () {
+  //add the current date to the currentDay id in the HTML
 
+  const currentDate = moment().format("dddd, MMMM Do YYYY, hh:mm a ");
+  $("#currentDay").html(currentDate);
+});
