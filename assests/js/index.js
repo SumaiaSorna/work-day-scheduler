939114c7cb772c05cdb98bf6d7e436c6529a96cc
SumaiaSorna <sorna.sumaia@gmail.com>
2021-10-14 09:08:32 +0100

    updated with pseudo code

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
-  //add the current date to the currentDay id in the HTML
-  const currentDate = moment().format("dddd, MMMM Do YYYY, hh:mm:ss a ");
-  $("#currentDay").html(currentDate);
+// $(document).ready(function () {
+//   //add the current date to the currentDay id in the HTML
+//   const currentDate = moment().format("dddd, MMMM Do YYYY, hh:mm:ss a ");
+//   $("#currentDay").html(currentDate);
 
-  //create html timeblocks with for loop starting at 9 am until 5pm
-});
+//create html timeblocks with for loop starting at 9 am until 5pm
+// });
 
 //making connection left side time and the time on the clock and change the color
 
@@ -28,3 +28,67 @@ $(document).ready(function () {
 
 //   timeBlock.map(construcktTimeBlock);
 // };
+
+const getCurrentDate = function () {
+  // use moment to get current date in format
+  // return formatted date
+};
+
+const renderCurrentDate = function (date) {
+  // target element and set text with date
+};
+
+const getClassName = function () {
+  return "past";
+};
+
+const getDataFromLS = function () {
+  // get from LS by key
+  // if it does not exist return {}
+  // else return data
+};
+
+const renderTimeBlocks = function () {
+  // declare time blocks
+  const timeBlockLabels = [
+    {
+      key: 9,
+      label: "09:00",
+    },
+  ];
+
+  const constructTimeBlock = function (timeBlock) {
+    // get the label for the time block 09:00
+    const label = timeBlock.label;
+
+    // get className for the specific time block
+    const className = getClassName();
+
+    // get text from LS for the specific time block
+    const planner = getDataFromLS();
+
+    // check if planner has text for the specific time block
+    // if yes then use the as the text else empty string
+
+    // construct your time block element in HTML
+
+    // return constructed time block element
+  };
+
+  timeBlockLabels.map(constructTimeBlock).join("");
+
+  // append HTML to parent
+};
+
+// function to call when document loads
+const onLoad = function () {
+  // render current date
+  const formattedDate = getCurrentDate();
+  renderCurrentDate(formattedDate);
+
+  // render time blocks
+  renderTimeBlocks();
+};
+
+// add load listener
+$(document).ready(onLoad);
